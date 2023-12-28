7195c6d0a70f1448c8bad7f23f044942645144c1
Dany <dany21sayan_@hotmail.com>
2023-12-28 09:37:00 +0000

    wip: checkpoint before changing db

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -1,7 +1,44 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from dj_rest_auth.views import PasswordResetConfirmView
+from core_apps.users.views import CustomUserDetailsView
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors API",
+        default_version="v1",
+        description="API endpoints for Authors API",
+        contact=openapi.Contact(email="leynad21_vicenzo@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    path("redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1/auth/user", CustomUserDetailsView.as_view(), name="user-details"),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path(
+        "api/v1/auth/registration/",
+        include("dj_rest_auth.registration.urls"),
+    ),
+    path(
+        "api/v1/auth/password/reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
+
+admin.site.site_header = "Authors API"
+
+admin.site.site_title = "Authors API Admin Portal"
+
+admin.site.index_title = "Authors API Admin"
--- core_apps/users/views.py
+++ core_apps/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .serializers import UserSerializer, CustomRegisterSerializer
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+class CustomUserDetailsView(RetrieveUpdateAPIView):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+    def get_object(self):
+        return self.request.user
+
+    def get_queryset(self):
+        return get_user_model().objects.none()
